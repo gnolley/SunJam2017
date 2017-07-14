e3c5c5629b0a679b7b44f2e2bf517240a7f73916
Thomas Knowles <thomasa.knowles@gmail.com>
2017-07-15 00:36:07 +1000

    Added player controller, added head rotation

---
+++ Project Elephant/Assets/Resources/Scripts/ElephantHead.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantHead : MonoBehaviour {
+
+
+    public enum DIRECTION { CounterClockwise, Clockwise };
+    public DIRECTION direction;
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateHead();	
+	}
+    
+    /// <summary>
+    /// Rotates the elephants head according to rotation direction
+    /// </summary>
+    public void rotateHead()
+    {
+        switch (direction)
+        {
+            case DIRECTION.CounterClockwise:
+                transform.RotateAround(Vector3.zero,transform.up, -rotationSpeed*Time.deltaTime);
+                break;
+            case DIRECTION.Clockwise:
+                transform.RotateAround(Vector3.zero, transform.up, rotationSpeed * Time.deltaTime);
+                break;
+        }
+        
+    }
+
+}
---
+++ Project Elephant/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string FORWARDKEY;
+    public string LEFTKEY;
+    public string RIGHTKEY;
+    public string BACKKEY;
+
+    public GameObject Target;
+
+    public float moveAmount;
+    public float rotationAmount;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        checkInput();
+	}
+
+    /// <summary>
+    /// Checks for input from the player
+    /// </summary>
+    void checkInput()
+    {
+        // More forward
+        if (Input.GetKeyDown(FORWARDKEY))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, moveAmount);
+        }
+        // More right
+        if (Input.GetKeyDown(RIGHTKEY))
+        {
+            transform.RotateAround(Vector3.zero, Target.transform.up, -rotationAmount);
+            transform.LookAt(Target.transform);
+        }
+        // More left
+        if (Input.GetKeyDown(LEFTKEY))
+        {
+            transform.RotateAround(Vector3.zero, Target.transform.up, rotationAmount);
+            transform.LookAt(Target.transform);
+        }
+        // More back
+        if (Input.GetKeyDown(BACKKEY))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, -Target.transform.position, moveAmount);
+        }
+    }
+
+}
